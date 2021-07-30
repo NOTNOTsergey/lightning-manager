fe06eda73b650bdcbfc84c5153e15a020013cc5a
sergey <sergejananev78@gmail.com>
2021-07-30 23:34:26 +0300

    absolute path

--- cli-test.py
+++ cli-test.py
@@ -3,5 +3,5 @@ from os import sys
 import os
 from db_api.table import Password
 
-db_api.global_init(db_api.sqlite_format_string('/home/sergey/password-manager/db/db.sqlite3'))
+db_api.global_init(db_api.sqlite_format_string('~/password-manager/db/db.sqlite3'))
 
