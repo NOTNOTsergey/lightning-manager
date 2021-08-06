c5823da6558a955b579bf11c36c4a609920565e8
sergey <sergejananev78@gmail.com>
2021-08-07 00:56:35 +0300

    launcher for 1st time

---
+++ first-launch.sh
@@ -0,0 +1,15 @@
+echo this is first launch of lightning manager
+
+echo ! creating database
+
+echo >> database
+
+echo ! installing python modules
+
+pip install -r requirments
+
+echo ! adding remote git repo
+
+git remote add lightning-manager https://github.com/NOTNOTsergey/lightning-manager
+
+echo finished...
